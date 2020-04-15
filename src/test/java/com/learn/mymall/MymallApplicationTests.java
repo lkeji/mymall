7ab6cdfe196e631112ccd8b8eb9afbd612de1b4c
kejiashan <297206795@qq.com>
2020-04-15 15:30:57 +0800

    first commit

---
+++ src/test/java/com/learn/mymall/MymallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.mymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
